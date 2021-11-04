1327fd865a07b6723a21e4ad0091e39d221126a2
Jimmy SOUSSAN <32173192+jilink@users.noreply.github.com>
2021-11-04 22:44:18 +0100

    Create github-to-vscode-toggle.js

---
+++ github-to-vscode-toggle.js
@@ -0,0 +1,14 @@
+javascript:(function(){
+	if (window.location.href.includes("vscode.dev")) {
+			window.location.replace(
+				window.location.href.replace(
+					"vscode.dev/",""
+					)
+					.replace("github","github.com"))
+
+	}
+	else {
+			window.location.replace(`http://vscode.dev/${window.location.href}`)
+	}
+
+})();
