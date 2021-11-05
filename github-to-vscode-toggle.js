1a6d3d58e0751f185f052eed848bd2ca1a9c9cd7
Jimmy SOUSSAN <32173192+jilink@users.noreply.github.com>
2021-11-05 12:51:30 +0100

    format

--- github-to-vscode-toggle.js
+++ github-to-vscode-toggle.js
@@ -1,14 +1,14 @@
-javascript:(function(){
+javascript: (function () {
 	if (window.location.href.includes("vscode.dev")) {
-			window.location.replace(
-				window.location.href.replace(
-					"vscode.dev/",""
-					)
-					.replace("github","github.com"))
+		window.location.replace(
+			window.location.href.replace(
+				"vscode.dev/", ""
+			)
+				.replace("github", "github.com"))
 
 	}
 	else {
-			window.location.replace(`http://vscode.dev/${window.location.href}`)
+		window.location.replace(`http://vscode.dev/${window.location.href}`)
 	}
 
 })();
